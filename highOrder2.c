4fa1ae7ae4cf0a0cea4a8fd4e956e7fc24ed8d1c
beyzaaydogan <beyzaaydogan026@gmail.com>
2019-10-31 15:16:10 +0300

    Can create master linkedList without duplicate

--- highOrder2.c
+++ highOrder2.c
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
-#define MAX 1024
+
 
 // self-referential structure                       
 struct listNode {                                      
@@ -33,75 +33,62 @@ void insertTofirstOrder(ListNodePtr *sPtr, char value[]);
 int main(){
 
         ListNodePtr startPtr = NULL; // initially there are no nodes
-        //DIR* FD;
         struct dirent **namelist;
         struct dirent **namelistSub;
-        //struct dirent* in_file;
-        FILE    *entry_file;
-        char words[MAX], c;
-        //char  buffer[BUFSIZ];
-    
-        int n, i, x, j ;
+        FILE    *inputTxtFile;
+        int numOfSubDirs, numOfSubFiles, j, i ;
 
-        n = scandir("./dataset", &namelist, NULL, alphasort); // . yerine /home/beyza/C-workspace/2025Projects gelebilir
+        numOfSubDirs = scandir("./dataset", &namelist, NULL, alphasort); // . yerine /home/beyza/C-workspace/2025Projects gelebilir
          
-        if (n < 0)
+        if (numOfSubDirs < 0)
             perror("scandir");
          
         else {
 
             char path[100] ="./dataset/"; 
-            printf("n is %d\n", n);
+            printf("number of sub directories are %d\n", numOfSubDirs);
             printf("%s\n", namelist[0]->d_name );
             printf("%s\n", namelist[1]->d_name );
             printf("%s\n", namelist[2]->d_name );
             printf("%s\n", namelist[3]->d_name );
             printf("%s\n", namelist[4]->d_name );
                                   
-              for( i = 2 ; i < n ; i++){
-                  char dirPath[100] , filePath[100] ;  
-                  strcpy(dirPath,path);
+              for( i = 2 ; i < numOfSubDirs ; i++){
+                  char dirPath[100], filePath[100];  
+                  strcpy(dirPath, path);
                   strcat(dirPath , namelist[i]->d_name );
-                  x = scandir(dirPath, &namelistSub, NULL, alphasort);
-                  //FD = opendir (onPath);
-                  //in_file  = readdir(FD);
+                  numOfSubFiles = scandir(dirPath, &namelistSub, NULL, alphasort);
+                 
 
-                  for(j = 2 ; j < x ; j++){
+                  for(j = 2 ; j < numOfSubFiles ; j++){
 
-                    //insert(&startPtr, item); // insert item in list
                     
                       printf("\n\n\n\n\nI am in %s\n\n",namelistSub[j]->d_name );
                       strcpy(filePath, dirPath);
-                      strcat(filePath,"/"); 
-                      strcat(filePath,namelistSub[j]->d_name); 
-                      printf("Filepath isss >>> %s\n\n\n\n", filePath );
-                      entry_file = fopen(filePath, "r");
-                      if (entry_file == NULL) { 
+                      strcat(filePath, "/"); 
+                      strcat(filePath, namelistSub[j]->d_name); 
+                      printf("Filepath is --> %s\n\n\n\n", filePath );
+                      inputTxtFile = fopen(filePath, "r");
+                      if (inputTxtFile == NULL) { 
                           printf("Cannot open file \n"); 
                           exit(0); 
                       } 
-                     /* c = fgetc(entry_file); burası dosyanın tamamını okumak için . 
-                      while (c != EOF) { 
-                          printf ("%c", c);         
-                          c = fgetc(entry_file); 
-                      } */
-
+                     
 
                       int count = 0;
-                      while((c = fgetc(entry_file)) != EOF){
-                        if(c == ' ' || c == '\n'){
-                          printf("\n");
-                          printf("word is >>>>  " );
-                          ++count;
+                     
+                      char word[40];
+                      while( fscanf(inputTxtFile,"%s", &word[0]) == 1 ){
+                        //puts("word is ----> ");
+                        //puts(word);
+                        insert(&startPtr, word); // insert item in list
+                        printList(startPtr);
+                        ++count;
                         }
-                        else{
-                          printf("%c", c);
-                        }
-                      }
-
-                      printf("This file has %d words in it.", count);
+                     
+                      printf("\nThis file has %d words in it.", count);
                     
-                      fclose(entry_file); 
+                      fclose(inputTxtFile); 
 
                    }
 
@@ -110,28 +97,6 @@ int main(){
 
          }
 
-/*
-           char item[80]; // char entered by user
-
-
-            unsigned int choice; // user's choice
-            scanf("%u", &choice);
-
-   // loop while user does not choose 3
-   while (choice != 3) { 
-
-            
-            printf("%s", "Enter a word: ");
-            scanf("\n%s", item);
-            printList(startPtr);
-            printf("%s", "? ");
-            scanf("%u", &choice);
-
-}
-
-
-
-*/
         
          return 1;
        }
@@ -152,6 +117,15 @@ int main(){
 
       ListNodePtr previousPtr = NULL;
       ListNodePtr currentPtr = *sPtr;
+
+      while(currentPtr != NULL){
+        if(strcmp(currentPtr->word, value) == 0){
+           return;
+        }
+        currentPtr = currentPtr->nextPtr;
+      }
+
+      currentPtr = *sPtr;
 
       // loop to find the correct location in the list       
       while (currentPtr != NULL && (int)value[0] > (int)currentPtr->word[0]) {
